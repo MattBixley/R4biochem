b6a0b2aaa1411191925a34197344153eef518f6f
Matt Bixley <mattbixley72@gmail.com>
2020-06-24 15:00:32 +1200

    pnas example

---
+++ code/test_code.R
@@ -0,0 +1,9 @@
+## packages used
+remotes::install_github("rstudio/rticles")
+tidyverse
+install.packages("ggthemes")
+
+## citation styles
+#https://www.zotero.org/styles
+
+https://yutannihilation.github.io/allYourFigureAreBelongToUs/
